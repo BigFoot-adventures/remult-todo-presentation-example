1e2aca8dd85d32c91339471e90c55fbd4afa4fc2
Brendon Jensen <brendoncjensen@gmail.com>
2022-12-06 16:12:51 -0500

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListViewComponent } from './list-view/list-view.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'profile/:userId',
+    component: LoginComponent,
+    children: [
+      {
+        path: 'lists',
+        component: ListViewComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
