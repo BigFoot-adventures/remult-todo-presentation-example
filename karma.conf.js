31eea6033c7e28d12167bc0b18171ead6f0dced9
Brendon Jensen <brendoncjensen@gmail.com>
2022-12-06 20:18:36 -0500

    updated app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/remult-angular-todo'),
+      dir: require('path').join(__dirname, './coverage/remult-todo-presentation-example'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'remult-angular-todo'`, () => {
+  it(`should have as title 'Remult-Example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('remult-angular-todo');
+    expect(app.title).toEqual('Remult-Example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('remult-angular-todo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Remult-Example app is running!');
   });
 });
