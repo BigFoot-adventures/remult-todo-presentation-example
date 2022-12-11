f59925e53a6e9d0d92a85e0d1e3639bc5c0085af
Brendon Jensen <brendoncjensen@gmail.com>
2022-12-11 16:33:47 -0500

    signup page ts

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { remult } from 'remult';
+import { User } from 'src/shared/User';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router) { }
+
+  userRepo = remult.repo(User);
+  userName = '';
+  firstName = '';
+  lastName = '';
+  password = '';
+
+  ngOnInit(): void {
+  }
+
+  async signUp(){
+    let newUser = new User(
+      this.userName,
+      this.firstName,
+      this.lastName,
+      this.password
+    );
+    let found = await this.userRepo.find({where: {userName: this.userName}});
+    if(found.length == 0){
+      const newuser = await this.userRepo.insert(newUser);
+    }else{
+      alert("Username in use");
+    }
+  }
+
+}
